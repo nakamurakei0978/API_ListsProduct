ed34db83b036848ddab3c1eca268a5ddeb5ea3a7
Kei <76953169+nakamurakei0978@users.noreply.github.com>
2023-12-07 13:20:10 +0700

    added cart

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,22 @@ import { RouterLink, RouterView } from 'vue-router'
 import Cart from './components/Cart.vue'
 
 const isToggleNav = ref(false)
-function toggleNav(){
-  isToggleNav.value=!isToggleNav.value
+function toggleNav() {
+  isToggleNav.value = !isToggleNav.value
 }
 
+const isCartToggle=ref(false)
+function cartToggle() {
+  isCartToggle.value = !isCartToggle.value
+}
 
 </script>
 
 <template>
+  
+
   <nav
-    class="backdrop-blur scroll-auto ps-3 pe-3 h-16 flex border-b border-slate-300/50 justify-between fixed top-0 w-full z-50">
+    class="backdrop-blur scroll-auto ps-3 pe-3 h-16 flex border-b border-slate-300/50 justify-between fixed top-0 w-full z-40">
     <div class="flex w-[250.91px]">
       <RouterLink to="/" class="p-5 text-[1.5]">Logo</RouterLink>
     </div>
@@ -23,23 +29,28 @@ function toggleNav(){
       <RouterLink active-class="border-b-2" to="/about" class="p-5 hover:border-b-2">About</RouterLink>
     </div>
     <div class="flex whitespace-nowrap max-md:hidden">
-      <button class="p-5 hover:border-b-2" @click="">Cart</button>
+      <button class="p-5 hover:border-b-2" @click="cartToggle">Cart</button>
       <button class="p-5 hover:border-b-2" @click="">Sign in</button>
       <button class="p-5 hover:border-b-2" @click="">Sign up</button>
     </div>
     <!-- small screen -->
-    <button :class="isToggleNav?'border-b-2':''" class="md:hidden p-5 hover:border-b-2" @click="toggleNav">
+    <button :class="isToggleNav ? 'border-b-2' : ''" class="md:hidden p-5 hover:border-b-2" @click="toggleNav">
       Menu
-      <Transition>
+      <Transition name="fade">
         <div class="h-[100dvh] overflow-y-scroll top-0 left-0 -z-10 absolute w-full bg-black/80" v-if="isToggleNav">
           <div class="h-16 bg-black/80 fixed w-full"></div>
           <div class="h-[100dvh] items-center pt-16 p-5 md:hidden flex flex-col">
-            <RouterLink active-class="border-b-2" to="/" class="p-5 shadow-inner border-2 border-transparent hover:border-b-2 hover:border-b-white">Home</RouterLink>
-            <RouterLink active-class="border-b-2" :to="{ name: 'product' }" class="p-5 border-2 border-transparent hover:border-b-2 hover:border-b-white">Product</RouterLink>
-            <RouterLink active-class="border-b-2" to="/about" class="p-5 border-2 border-transparent hover:border-b-2 hover:border-b-white">About</RouterLink>
-            <button class="p-5 border-2 border-transparent hover:border-b-2 hover:border-b-white" @click="">Cart</button>
-            <button class="p-5 border-2 border-transparent hover:border-b-2 hover:border-b-white" @click="">Sign in</button>
-            <button class="p-5 border-2 border-transparent hover:border-b-2 hover:border-b-white" @click="">Sign up</button>
+            <RouterLink active-class="border-b-2" to="/"
+              class="p-5 shadow-inner border-2 border-transparent hover:border-b-2 hover:border-b-white">Home</RouterLink>
+            <RouterLink active-class="border-b-2" :to="{ name: 'product' }"
+              class="p-5 border-2 border-transparent hover:border-b-2 hover:border-b-white">Product</RouterLink>
+            <RouterLink active-class="border-b-2" to="/about"
+              class="p-5 border-2 border-transparent hover:border-b-2 hover:border-b-white">About</RouterLink>
+            <button class="p-5 border-2 border-transparent hover:border-b-2 hover:border-b-white" @click="cartToggle">Cart</button>
+            <button class="p-5 border-2 border-transparent hover:border-b-2 hover:border-b-white" @click="">Sign
+              in</button>
+            <button class="p-5 border-2 border-transparent hover:border-b-2 hover:border-b-white" @click="">Sign
+              up</button>
           </div>
         </div>
       </Transition>
@@ -47,22 +58,35 @@ function toggleNav(){
   </nav>
   <div class="container mx-auto px-4">
     <div class="mt-16 min-h-screen pb-20">
-      <RouterView/>
+      <RouterView />
+      <Transition name="cart">
+        <Cart v-if="isCartToggle" @cart-toggle="(msg)=>isCartToggle=msg"/>
+      </Transition>
     </div>
-    <!-- <Cart /> -->
   </div>
   <footer class="h-52 -mt-16 border-t border-slate-300/50">
     ok
   </footer>
 </template>
 <style scoped>
-.v-enter-active,
-.v-leave-active {
+.fade-enter-active,
+.fade-leave-active {
   transition: opacity 0.3s ease;
 }
 
-.v-enter-from,
-.v-leave-to {
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+
+.cart-enter-active,
+.cart-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.cart-enter-from,
+.cart-leave-to {
   opacity: 0;
 }
 </style>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -3,9 +3,29 @@ import PageTitle from '../components/PageTitle.vue';
 import ProdCard from '../components/ProdCard.vue';
 import { ref, inject } from 'vue';
 import SectionTitle from '../components/SectionTitle.vue';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/grid';
+import 'swiper/css/pagination';
+import { Grid, Pagination } from 'swiper/modules';
 
 const commerce = inject('commerce');
 const props = defineProps({ class: String })
+
+const catLength = ref(0)
+const categories = ref([])
+const isCatReady = ref(null)
+async function fetchCategory() {
+  isCatReady.value = null
+  // use await to get the value of the promise
+  const res = await commerce.categories.list();
+  console.log(res.data);
+  categories.value = res.data
+  catLength.value = categories.value.length
+  isCatReady.value = true
+}
+fetchCategory()
 
 const isProdReady = ref(null)
 const prodLength = ref(0)
@@ -25,6 +45,52 @@ fetchProducts()
 
 <template>
   <PageTitle class="pt-5 text-3xl text-center" title="Products" />
+
+  <SectionTitle title="Category" :total="catLength" />
+  <div>
+    <div v-if="!isCatReady"
+      class="grid overflow-hidden gap-5 animate-pulse sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      <div v-for="i in 5" class="w-full bg-slate-400/25 gap-5 h-[250px] p-2 ">
+        <div class="bg-white/25 h-[210px] w-full inline-block"></div>
+        <p class="bg-white/25 h-4 inline-block w-28"></p>
+      </div>
+    </div>
+
+    <swiper :style="{
+      '--swiper-pagination-color': 'Black',
+    }" :breakpoints="{
+  0: {
+    slidesPerView: 1,
+    spaceBetween: 10
+  },
+  640: {
+    slidesPerView: 2,
+    spaceBetween: 10
+  },
+  768: {
+    slidesPerView: 3,
+    spaceBetween: 10
+  },
+  1024: {
+    slidesPerView: 4,
+    spaceBetween: 20
+  },
+  1280: {
+    slidesPerView: 5,
+    spaceBetween: 20
+  }
+}" :slidesPerView="6" :grid="{
+  rows: 1,
+}" :pagination="{
+  type: 'progressbar',
+}" :spaceBetween="30" :modules="modules" class="mySwiper">
+      <swiper-slide class="cursor-pointer " v-for="category in categories">
+        <img :src="category.assets[0].url" alt="">
+        <p>{{ category.name }}</p>
+      </swiper-slide>
+    </swiper>
+  </div>
+
   <SectionTitle title="Products" :total="prodLength" />
   <div>
     <div v-if="!isProdReady"
